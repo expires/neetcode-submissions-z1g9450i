20bfde6916882dd1f2e4eec722ab693b9930acd1
Yath M <73852745+expires@users.noreply.github.com>
2026-05-23 23:22:16 +0100

    Add: partition-labels - submission-3

---
+++ Data Structures & Algorithms/partition-labels/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char,int> freq;
+        for (int i = 0; i < s.size(); i++)
+        {
+            freq[s[i]] = i;
+        }
+
+
+        int start = 0;
+        int end = freq[s[0]];
+
+        vector<int> res;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            end = max(end, freq[s[i]]);
+            if (i == end) 
+            {
+                res.push_back((i - start + 1));
+                start = i + 1;
+                continue;
+            }
+            
+        }
+
+        return res;
+
+    }
+};
